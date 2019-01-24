df99a7963c6dc1f45e242398f6b34a72a04976a6
swechchhaparajuli <swechchha@gmail.com>
2019-01-24 00:03:50 -0800

    Updates

---
+++ precache-manifest.546db2ea3f54c14a4a7af327d888318d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5e1124212e913e930db",
+    "url": "/clickyGame/static/js/runtime~main.f5e11242.js"
+  },
+  {
+    "revision": "544d7375ca906274a850",
+    "url": "/clickyGame/static/js/main.544d7375.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/clickyGame/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "544d7375ca906274a850",
+    "url": "/clickyGame/static/css/main.cbb29b2c.chunk.css"
+  },
+  {
+    "revision": "0c6b0c43532f329a8c3147cd5cb0818a",
+    "url": "/clickyGame/index.html"
+  }
+];
