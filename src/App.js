1c704cbe5e13b0783ca743d97ca58c93f6a5e461
swechchhaparajuli <swechchha@gmail.com>
2019-01-23 20:37:34 -0800

    the shuffle is working

--- src/App.js
+++ src/App.js
@@ -7,21 +7,19 @@ import friends from "./friends.json";
 class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
-    friends,
-
+    friends
   };
 
-  shuffleFriend = id => {
+  shuffle = id => {
 
   var a = this.state.friends;
+  
   for (let i = a.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [a[i], a[j]] = [a[j], a[i]];
 }
-    // Filter this.state.friends for friends with an id not equal to the id being removed
-    //const friends = this.state.friends.filter(friend => friend.id !== id);
-    //console.log(friends);
-    // Set this.state.friends equal to the new friends array
+
+
     this.setState({ a });
   };
 
@@ -33,21 +31,19 @@ class App extends Component {
         <Title>Hogwarts Clicky Game</Title>
         {this.state.friends.map(friend => (
           <FriendCard
-            removeFriend={this.shuffleFriend}
+            shuffle={this.shuffle}
             id={friend.id}
             key={friend.id}
             name={friend.name}
             image={friend.image}
             house={friend.house}
-            location={friend.location}
+            checked={1}
           />
         ))}
       </Wrapper>
     );
   }
 }
-
-
 
 
 export default App;
